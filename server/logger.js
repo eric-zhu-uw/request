f1a702f71700850e57dc3bc856b68b075a22a9b5
eric-zhu <ericzhu77@gmail.com>
2018-02-18 05:47:25 -0800

    moved port and logger to config with possible flags in webpack still

--- server/logger.js
+++ server/logger.js
@@ -4,5 +4,8 @@ import config from './config';
 const logger = new winston.Logger({
   transports: [new winston.transports.Console(config.winston)],
 });
+logger.level = process.env.LOGGER_LEVEL
+  ? process.env.LOGGER_LEVEL
+  : config.logger.level;
 
 export default logger;
